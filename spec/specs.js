fb551249b7b313060fe509a57d03a71689688295
Cody Bouscaren <codybousc@gmail.com>
2015-05-29 12:07:22 -0700

    Passes replace word spec

--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,7 @@ describe('wordReplace', function() {
   it("remove's a user provided word from a user provided sentence", function() {
     expect(wordReplace("Tbone walks on water", "water", "")).to.equal("Tbone walks on ");
   });
+  it("replaces a user provided word in a user provided sentence", function() {
+    expect(wordReplace("Tbone walks on soil", "soil", "water")).to.equal("Tbone walks on water");
+  });
 });

4180df30ad8b0d28e5c33fc634e40b48f37d3177
Cody Bouscaren <codybousc@gmail.com>
2015-05-29 12:46:57 -0700

    Passes search and initial letter of sentence case specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,34 @@
+function capitalize(s)
+{
+    return s[0].toUpperCase() + s.slice(1);
+};
+
+
 var wordReplace = function(sentence, wordFind, wordReplace) {
-var s = sentence;
-var wf = wordFind;
+var s = sentence.toLowerCase();
+var wf = wordFind.toLowerCase();
 var wr = wordReplace;
 
 var new_sen = s.replace(wf, wr);
-return new_sen;
+var final_sen = capitalize(new_sen);
+return final_sen;
 };
+
+
 
 
 $(document).ready(function(){
   $("form#replacer").submit(function(event){
-    alert("Got to beginning of form");
     var user_sentence_init = $("input#user_sentence").val();
     var user_word_init = $("input#find_word").val();
     var user_word_replace = $("input#replace_word").val();
     var new_string = wordReplace(user_sentence_init, user_word_init, user_word_replace);
 
 
-      $(".new_sentence").text(new_string);
-      $("#result").show();
+
+
+    $(".new_sentence").text(new_string);
+    $("#result").show();
 
     event.preventDefault();
   });
--- spec/specs.js
+++ spec/specs.js
@@ -3,7 +3,17 @@ describe('wordReplace', function() {
   it("remove's a user provided word from a user provided sentence", function() {
     expect(wordReplace("Tbone walks on water", "water", "")).to.equal("Tbone walks on ");
   });
+
   it("replaces a user provided word in a user provided sentence", function() {
     expect(wordReplace("Tbone walks on soil", "soil", "water")).to.equal("Tbone walks on water");
   });
+
+  it("replaces a user provided word regardless of capitalization", function() {
+    expect(wordReplace("Tbone walks on land", "Land", "water")).to.equal("Tbone walks on water");
+  });
+
+  it("capitalizes the first letter of a sentence regardless of how it was entered", function() {
+    expect(wordReplace("tbone walks on land", "land", "water")).to.equal("Tbone walks on water");
+  });
+
 });
